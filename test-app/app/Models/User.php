dbab7b456591a937d158ae0f01f3fb05c18c8f80
Evgeniy <emrindya@gmail.com>
2025-01-08 13:11:09 +0300

    fix models

--- test-app/app/Models/User.php
+++ test-app/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Model
 {
@@ -11,7 +12,7 @@ class User extends Model
 
     protected $fillable = ['name', 'email', 'active'];
 
-    public function groups()
+    public function groups(): BelongsToMany
     {
         return $this->belongsToMany(Group::class)->withPivot('expired_at');
     }
