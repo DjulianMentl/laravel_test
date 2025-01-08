df2f87e8541b705e71d4f8bec1a12ccdb568cd5b
Evgeniy <emrindya@gmail.com>
2025-01-08 14:15:29 +0300

    add controllers and resources

---
+++ test-app/app/Http/Controllers/Api/GroupController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\GroupResource;
+use App\Models\Group;
+use Illuminate\Http\Request;
+
+class GroupController extends Controller
+{
+    public function store(Request $request): GroupResource
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'expire_hours' => 'required|integer',
+        ]);
+
+        $group = Group::create($validated);
+
+        return new GroupResource($group);
+    }
+
+    public function show(Group $group): GroupResource
+    {
+        return new GroupResource($group);
+    }
+
+    public function update(Request $request, Group $group): GroupResource
+    {
+        $validated = $request->validate([
+            'name' => 'string|max:255',
+            'expire_hours' => 'integer',
+        ]);
+
+        $group->update($validated);
+
+        return new GroupResource($group);
+    }
+}
---
+++ test-app/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function store(Request $request): UserResource
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'active' => 'boolean',
+        ]);
+
+        $user = User::create($validated);
+
+        return new UserResource($user);
+    }
+
+    public function show(User $user): UserResource
+    {
+        return new UserResource($user);
+    }
+
+    public function update(Request $request, User $user): UserResource
+    {
+        $validated = $request->validate([
+            'name' => 'string|max:255',
+            'email' => 'email|max:255|unique:users,email,' . $user->id,
+            'active' => 'boolean',
+        ]);
+
+        $user->update($validated);
+
+        return new UserResource($user);
+    }
+}
